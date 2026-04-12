30d0779e45b77f07dbc39a08e3841ff08874fbd5
sandeladevan <sandeladevan@gmail.com>
2026-04-12 12:21:45 +0100

    Added DHT11 sensor reading

--- firmware/esp32_wifi_test/esp32_wifi_test.ino
+++ firmware/esp32_wifi_test/esp32_wifi_test.ino
@@ -1,12 +1,24 @@
 #include <WiFi.h>
 #include "secrets.h"
+#include "DHT.h"
 
+// wifi config
 const char* ssid = WIFI_SSID;
 const char* password = WIFI_PASSWORD;
+
+// sensor config
+#define DHTPIN 4
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   Serial.begin(115200);
 
+// start sensor
+dht.begin();
+
+//connect wifi
   WiFi.begin(ssid, password);
 
   Serial.print("Connecting to WiFi");
@@ -22,5 +34,19 @@ void setup() {
 }
 
 void loop() {
-  // nothing here
+  float temp = dht.readTemperature();
+  float hum = dht.readHumidity();
+
+  // check if reading failed
+  if (isnan(temp) || isnan(hum)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Serial.print("Temperature: ");
+  Serial.print(temp);
+  Serial.print(" °C | Humidity: ");
+  Serial.println(hum);
+
+  delay(2000);  // DHT11 needs delay
 }
---
+++ firmware/esp32_wifi_test/secrets.h
@@ -0,0 +1,7 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+#define WIFI_SSID "eir32745728"
+#define WIFI_PASSWORD "rrMpAvuSKN"
+
+#endif
